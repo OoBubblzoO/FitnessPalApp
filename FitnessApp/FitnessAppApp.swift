41544b3a93d4a6dcbec2343e1167d8665b5dba52
Pedro Negron <pedronegron3rd@gmail.com>
2026-02-10 17:06:35 -0800

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Cheeto on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
